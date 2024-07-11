bf06476eb9ef092bc65ef231db5967718809c436
Priyajan Swongamikha <aswongamikha@gmail.com>
2024-07-11 21:36:30 +0545

    Added a functionality to save uploaded file in pwd, Used PyPDFLoader to solve pdf problem, problem->not all pdf opened

--- test.py
+++ test.py
@@ -3,8 +3,18 @@ import tabula as tb;
 from fastembed import TextEmbedding
 from typing import List
 import numpy as np
+from langchain_community.document_loaders import PyPDFLoader
+import os
 
 embedding_model = TextEmbedding()
+def save_uploadedfile(uploadedfile):
+    try:
+            with open(os.path.join("./",uploadedfile.name),"wb") as f:
+                f.write(uploadedfile.getbuffer())
+                st.success("Saved File:{} to tempDir".format(uploadedfile.name))
+            return uploadedfile.name
+    except:
+        return 0;
 
 st.write("this is test");
 # this either returns single file or none, if accept multiple file is true then it returns list or none
@@ -12,15 +22,16 @@ uploadFile= st.file_uploader("upload your book here",type="pdf",key="uploaded")
 
 # uploaded file
 if uploadFile:
-    book=tb.read_pdf(uploadFile);
-    print(book);
-
-
-
+    fileName=save_uploadedfile(uploadFile)
+    if fileName:
+        book=PyPDFLoader("./"+fileName)
+        # print(book)
+        pages=book.load_and_split();
+        print(pages)
 
 chat=st.chat_input("what's on your mind");
 
-testWord="car";
+testWord="hey";
 if chat:
     embedding_generator=embedding_model.embed(chat);
     vectorData=list(embedding_generator);
@@ -31,6 +42,6 @@ if chat:
     result=np.dot(vectorData[0],testVectorData[0]);
     print(result)
     testVectorData=np.array(testVectorData);
-    # print(testVectorData[0])
+    # print(testVectorData[0].shape)
 
 # print(vectorData);
