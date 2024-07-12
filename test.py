4500899e6763f8999470d7698ebb9c4bd70b704e
Priyajan Swongamikha <aswongamikha@gmail.com>
2024-07-13 00:10:11 +0545

    added qudrant vector database, added a function to implement embedding of pages for page content and metadata in vector data base, also implemented the search function, problem: multivector is not found in the collection

--- test.py
+++ test.py
@@ -1,12 +1,24 @@
 import streamlit as st;
 import tabula as tb;
 from fastembed import TextEmbedding
-from typing import List
+
 import numpy as np
 from langchain_community.document_loaders import PyPDFLoader
 import os
+from qdrant_client import QdrantClient;
+from qdrant_client.http.models import VectorParams, PointStruct
+
+# init client
+qClient=QdrantClient(":memory:");
+# review this code below
+collection_name = "Operator_Overloading"
+qClient.recreate_collection(
+    collection_name=collection_name,
+    vectors_config=VectorParams(size=384, distance="Cosine")
+)
 
 embedding_model = TextEmbedding()
+# this is a function to save file locally
 def save_uploadedfile(uploadedfile):
     try:
             with open(os.path.join("./",uploadedfile.name),"wb") as f:
@@ -27,21 +39,43 @@ if uploadFile:
         book=PyPDFLoader("./"+fileName)
         # print(book)
         pages=book.load_and_split();
-        print(pages)
+        i=0;
+        for page in pages:
+            # Generate embedding for the page content
+            embedding = embedding_model.embed(page.page_content)
+            vector_data = list(embedding)
+            # review this code below
+            # Create a point structure with the document and metadata
+            point = PointStruct(id=i, vector=vector_data, payload={"content": page.page_content, "metadata": page.metadata})
+            
+            # Add point to the collection
+            qClient.upsert(collection_name=collection_name, points=[point])
+            
+            print("iteration:", i)
+            i += 1
+        # print(pages[1].page_content)
 
 chat=st.chat_input("what's on your mind");
 
-testWord="hey";
+# testWord="what is a function overloading?";
 if chat:
     embedding_generator=embedding_model.embed(chat);
     vectorData=list(embedding_generator);
 
-    embedding_generator2=embedding_model.embed(testWord);
-    testVectorData=list(embedding_generator2)
+    # embedding_generator2=embedding_model.embed(testWord);
+    # testVectorData=list(embedding_generator2)
+
+    search_result=qClient.search(
+         collection_name="Operator_Overloading",
+        #  query_text=chat,
+         query_vector=vectorData,
+         limit=5
+    )
+    print(search_result)
     # checking the logic to match two words
-    result=np.dot(vectorData[0],testVectorData[0]);
-    print(result)
-    testVectorData=np.array(testVectorData);
+    # result=np.dot(vectorData[0],testVectorData[0]);
+    # print(result)
+    # testVectorData=np.array(testVectorData);
     # print(testVectorData[0].shape)
 
 # print(vectorData);
