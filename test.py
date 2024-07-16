517a503e37fdeaeaf9bd715636c6e50667e85934
Priyajan Swongamikha <aswongamikha@gmail.com>
2024-07-16 14:44:14 +0545

    Integrated LLM to generate response based on retrieved info

--- test.py
+++ test.py
@@ -1,102 +1,120 @@
 import streamlit as st;
-import tabula as tb;
-from fastembed import TextEmbedding
-
-import numpy as np
 from langchain_community.document_loaders import PyPDFLoader
+from fastembed import TextEmbedding
 import os
-from qdrant_client import QdrantClient;
-from qdrant_client.http.models import VectorParams, PointStruct
+from qdrant_client import QdrantClient
+from qdrant_client.http.models import VectorParams,PointStruct
 from openai import OpenAI
+from dotenv import load_dotenv
+from qdrant_client import QdrantClient
 
-key="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"
+load_dotenv()
+print("om")
+# file_in_session=False
+
+
+db = QdrantClient(
+    url="https://149e4918-9505-4d8d-bf3c-fb96c39318aa.us-east4-0.gcp.cloud.qdrant.io:6333", 
+    api_key=os.getenv("qdrant_key"),
+)
+
 # define model
  # ollama pull phi3:3.8b-instruct
 # SYSTEM_MESSAGE = {"role": "system", "content": "You are a helpful assistant."}
+MODEL = "llama3-8b-8192" 
 client = OpenAI(
     base_url="https://api.groq.com/openai/v1/",  # ollama endpoint
-    api_key=key,
+    api_key=os.getenv("key"),
 )
 
+# if 'file_processed' not in st.session_state:
+#     st.session_state.file_processed = False
 
 
-# init client
-
-qClient=QdrantClient(":memory:");
-# collection_name
-collection_name = "Operator_Overloading"
-# review this code below
-qClient.recreate_collection(
-    collection_name=collection_name,
-    vectors_config=VectorParams(size=384, distance="Cosine")
+db.recreate_collection(
+    collection_name="books",
+    vectors_config=VectorParams(size=384,distance="Cosine")
 )
-
-embedding_model = TextEmbedding()
-# this is a function to save file locally
-def save_uploadedfile(uploadedfile):
+def save_uploads(upload):
     try:
-            with open(os.path.join("./",uploadedfile.name),"wb") as f:
-                f.write(uploadedfile.getbuffer())
-                st.success("Saved File:{} to tempDir".format(uploadedfile.name))
-            return uploadedfile.name
+        with open(os.path.join("./"+upload.name), "wb") as f:
+            f.write(upload.getbuffer());
+            st.success("saved sucessfully :"+(upload.name));
+            book=PyPDFLoader(os.path.join("./"+upload.name))
+            return book
     except:
+        st.error("file could not be saved properly")
         return 0;
 
-st.write("this is test");
-# this either returns single file or none, if accept multiple file is true then it returns list or none
-uploadFile= st.file_uploader("upload your book here",type="pdf",key="uploaded")
-
-# uploaded file
-if uploadFile:
-    fileName=save_uploadedfile(uploadFile)
-    if fileName:
-        book=PyPDFLoader("./"+fileName)
-        # print(book)
-        pages=book.load_and_split();
-        i=0;
+def push_vector_db(book):
+    pages=book.load_and_split();
+    id=0
+    try:
         for page in pages:
-            # Generate embedding for the page content
-            embedding = embedding_model.embed(page.page_content)
-            vector_data = list(embedding)
-            # review this code below
-            # Create a point structure with the document and metadata
-            
-            point = PointStruct(id=i, vector=vector_data[0], payload={"content": page.page_content, "metadata": page.metadata})
-            
-            # Add point to the collection
-            qClient.upsert(collection_name=collection_name, points=[point])
-            
-            print("iteration:", i)
-            i += 1
-        # print(pages[1].page_content)
+            page2vector=embeding_model.embed(page.page_content);
+            page2vector=list(page2vector)
+            point=PointStruct(id=id, vector=page2vector[0],payload={"content":page.page_content, "metadata":page.metadata})
+            db.upsert(collection_name="books",points=[point])
+            print("iteration:",id)
+            id+=1
+        # st.session_state.file_processed = True        
+        
+    except:
+        st.error("lol error in saving book")
 
-chat=st.chat_input("what's on your mind");
+embeding_model=TextEmbedding();
+st.write("This is an attempt to recreate the project from scratch")
 
-# testWord="what is a function overloading?";
+
+upload=st.file_uploader("Upload your file",type="pdf",key="uploaded_file")
+
+
+
+
+chat=st.chat_input("write your message here")
+
+
+if upload:
+    book = save_uploads(upload)
+    if book:
+        push_vector_db(book)
+
 if chat:
-    embedding_generator=embedding_model.embed(chat);
-    vectorData=list(embedding_generator);
-    # print(vectorData[0])
-
-    # embedding_generator2=embedding_model.embed(testWord);
-    # testVectorData=list(embedding_generator2)
-
-    search_result=qClient.search(
-         collection_name="Operator_Overloading",
-        #  query_text=chat,
-         query_vector=vectorData[0],
-         limit=5
+    chat2vector=list(embeding_model.embed(chat))
+    search_results=db.search(
+        collection_name="books",
+        query_vector=chat2vector[0],
+        limit=5
     )
-    
-    output=[result.payload['content'] for result in search_result]
-    st.write(output)
-    
-    
-    # print(test[0])
-    # checking the logic to match two words
-    # result=np.dot(vectorData[0],testVectorData[0]);
-    # print(result)
-    # testVectorData=np.array(testVectorData);
-    # print(testVectorData[0].shape)
+    output=[result.payload["content"] for result in search_results ]
+    # st.write(output)
+    # apparently every single content needs a role
+    prompt = [
+            {
+                "role": "system",
+                "content": chat +" and also answer the following question using the context below"
+            },
+            
+        ]
+    for message in output:
+        prompt.append({
+            "role":"user",
+            "content":message
+        })
+    print(prompt)
+    reply_stream=client.chat.completions.create(
+        model=MODEL,
+        # this message does not take dict but list of dictionary 
+        messages= prompt,
+        stream=True
+    )
+    st.write(reply_stream)
 
-# print(vectorData);
+
+
+
+    
+
+
+
+
